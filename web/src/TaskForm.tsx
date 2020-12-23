6cf2f3c1befdfc8897fd2209681fc434bbad102c
FScoward <endlick@gmail.com>
2020-12-23 22:06:51 +0900

    フォームを試しに作る

---
+++ web/src/TaskForm.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+type State = { value: string }
+
+class TaskForm extends React.Component<{}, State> {
+  constructor(props: {}) {
+    super(props);
+    this.state = { value: '' };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event: any) {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit(event: any) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+
+export default TaskForm;
--- web/src/index.tsx
+++ web/src/index.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import TaskForm from './TaskForm';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <TaskForm />
   </React.StrictMode>,
   document.getElementById('root')
 );
