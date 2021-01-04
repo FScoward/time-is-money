b449c0da808d729b5fb5e6ad37f294c3c351d3eb
FScoward <endlick@gmail.com>
2021-01-05 00:16:20 +0900

    adopt button color

--- web/src/components/TaskCard.tsx
+++ web/src/components/TaskCard.tsx
@@ -24,6 +24,19 @@ const useStyles = makeStyles({
   pos: {
     marginBottom: 12,
   },
+  button: {
+    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+    border: 0,
+    borderRadius: 3,
+    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+    color: 'white',
+    height: 48,
+    padding: '0 30px',
+  },
+});
+
+const buttonStyles = makeStyles({
+
 });
 
 export default function TaskCard(props: { task: Task, taskHandler: ((newWipTask: WIPTask) => void) }) {
@@ -56,7 +69,7 @@ export default function TaskCard(props: { task: Task, taskHandler: ((newWipTask:
         <Timer />
       </CardContent>
       <CardActions>
-        <Button onClick={startTimerHandler} size="small">Start Timer</Button>
+        <Button className={classes.button} onClick={startTimerHandler} size="small">Start Timer</Button>
       </CardActions>
     </Card>
   );
--- web/src/components/WIPTaskCard.tsx
+++ web/src/components/WIPTaskCard.tsx
@@ -24,13 +24,23 @@ export default function WIPTaskCard(props: { task: WIPTask, taskHandler: ((task:
     pos: {
       marginBottom: 12,
     },
+    button: {
+      background: 'linear-gradient(45deg, #2196F3 30%, #21CBF3 90%)',
+      border: 0,
+      borderRadius: 3,
+      boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+      color: 'white',
+      height: 48,
+      padding: '0 30px',
+    },
+
   });
 
   const classes = useStyles();
 
   function stopTimerHandler() {
     let record = {
-      startTime: props.task.startTime, 
+      startTime: props.task.startTime,
       endTime: new Date()
     } as TimeRecord
     let stopTask = {
@@ -49,7 +59,7 @@ export default function WIPTaskCard(props: { task: WIPTask, taskHandler: ((task:
         タイマー開始：{props.task.startTime.toLocaleTimeString()}
       </CardContent>
       <CardActions>
-        <Button onClick={stopTimerHandler} size="small">Stop Timer</Button>
+        <Button className={classes.button} onClick={stopTimerHandler} size="small">Stop Timer</Button>
       </CardActions>
     </Card>
   );
